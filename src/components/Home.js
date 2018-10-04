98cac42ca0a32011c6dce853d9918d9d6a195ad8
Billy Tseng <tseng.billy@gmail.com>
2018-10-03 21:40:38 -0700

    feat: add gallery.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Button, Spin } from 'antd';
 import { GEO_OPTIONS, POS_KEY } from "../constant";
+import { Gallery } from "./Gallery"
 
 const TabPane = Tabs.TabPane;
 
@@ -45,7 +46,7 @@ export class Home extends React.Component {
         } else if (this.state.loadGeoLocation) {
             return <Spin tip="Loading geo locations..."/>;
         } else {
-            return null;
+            return <Gallery  images={imageList}/>;
         }
     }
 
@@ -60,3 +61,70 @@ export class Home extends React.Component {
         );
     }
 }
+
+const imageList = [
+    {
+        user: 'Billy',
+        src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
+        thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
+        thumbnailWidth: 271,
+        thumbnailHeight: 320,
+        caption: "Orange Macro (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'Billy',
+        src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
+        thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 190,
+        caption: "286H (gratisography.com)"
+    },
+    {
+        user: 'Billy',
+        src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
+        thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 148,
+        caption: "315H (gratisography.com)"
+    },
+    {
+        user: 'Billy',
+        src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
+        thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 213,
+        caption: "201H (gratisography.com)"
+    },
+    {
+        user: 'Billy',
+        src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
+        thumbnailWidth: 248,
+        thumbnailHeight: 320,
+        caption: "Big Ben (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'Billy',
+        src: "https://c1.staticflickr.com/9/8785/28687743710_870813dfde_h.jpg",
+        thumbnail: "https://c1.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 113,
+        caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'Billy',
+        src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
+        thumbnail: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_n.jpg",
+        thumbnailWidth: 313,
+        thumbnailHeight: 320,
+        caption: "Wood Glass (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'Billy',
+        src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
+        thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 213,
+        caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)"
+    }
+];
